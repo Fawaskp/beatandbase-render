3f84a2ec30dd2564288fab0dba7c27a7fd51c2e2
Fawas <fawaskp1010@gmail.com>
2023-10-11 14:31:45 +0530

    staic root added

--- beatandbase/urls.py
+++ beatandbase/urls.py
@@ -39,6 +39,8 @@ urlpatterns = [
     path('orders/', include('orders.urls')),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
     
 handler400 = views.error_404
